d1e816417132e03d321f2d06e550f9f4a3bc41ad
Ezequiel Braga <ezequiel.braga.santos@gmail.com>
2026-05-04 16:01:44 -0300

    add city level data

---
+++ data_prep/sel_cities.r
@@ -0,0 +1,53 @@
+library(tidyverse)
+
+cities_dengue <- c(
+  2931350,
+  2933307,
+  2302503,
+  3119401,
+  3549805,
+  3541406,
+  1200401,
+  1200203,
+  1716109,
+  4113700,
+  4103701,
+  4104808,
+  5201405,
+  5102637,
+  5215231
+)
+
+cities_chikungunya <- c(
+  2211001,
+  2931350,
+  3143302,
+  3119401,
+  1721000,
+  1716109,
+  4104808,
+  4219507,
+  5103403,
+  5102637
+)
+
+dengue_merged <- read_csv("processed_data/dengue/dengue_merged.csv.gz")
+chikungunya_merged <- read_csv("processed_data/chikungunya/chikungunya_merged.csv.gz")
+
+lapply(cities_dengue, function(city) {
+  if (!dir.exists("processed_data/dengue/sel_cities")) {
+    dir.create("processed_data/dengue/sel_cities")
+  }
+  dengue_merged %>%
+    filter(geocode == city) %>%
+    write_csv(paste0("processed_data/dengue/sel_cities/dengue_", city, ".csv.gz"))
+})
+
+lapply(cities_chikungunya, function(city) {
+  if (!dir.exists("processed_data/chikungunya/sel_cities")) {
+    dir.create("processed_data/chikungunya/sel_cities")
+  }
+  chikungunya_merged %>%
+    filter(geocode == city) %>%
+    write_csv(paste0("processed_data/chikungunya/sel_cities/chikungunya_", city, ".csv.gz"))
+})
